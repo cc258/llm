cbcf45f990546a8cf54020a3cd73e35dfb648354
Charlie <cc258@qq.com>
2026-08-07 19:45:19 +0800

    fixed: database not found

--- rag/config_data.py
+++ rag/config_data.py
@@ -7,6 +7,7 @@ EMBEDDING_MODEL_NAME = 'qwen3.8-max'
 KNOWLEDGE_BASE_TXT = os.path.join(BASE_DIR, 'md5.txt')
 KNOWLEDGE_BASE_COLLECTION_NAME = 'rag'
 KNOWLEDGE_PERSISTENT_DIR = os.path.join(BASE_DIR, 'chroma_db')
+os.makedirs(KNOWLEDGE_PERSISTENT_DIR, exist_ok=True)
 
 chunk_size = 150
 chunk_overlap = 30
